6786374173472eab92ce0fd194b4ef2ccd2245a9
Zolorah <maxime.bosca@gmail.com>
2024-10-07 13:05:45 +0200

    Removing crafting time from RecipeComponent (renamed from RecipeItem)

--- src/satisfactoryrecipe/recipe.py
+++ src/satisfactoryrecipe/recipe.py
@@ -3,26 +3,25 @@ from .item import Item
 from typing import List, Optional
 
 
-class RecipeItem:
-    def __init__(self, item: Item, quantity: int, crafting_time: int):
+class RecipeComponent:
+    def __init__(self, item: Item, quantity: int):
         self.item = item
         self.quantity = quantity
-        self.quantity_per_minute = (60/crafting_time)*quantity if crafting_time > 0 else 0
 
 
 class Recipe:
-    inputs: List[RecipeItem]
-    outputs: List[RecipeItem]
+    inputs: List[RecipeComponent]
+    outputs: List[RecipeComponent]
 
     def __init__(self, name: str, machine: Optional[Machine] = None, crafting_time: Optional[int] = None):
         self.name, self.machine, self.crafting_time = name, machine, crafting_time
         self.inputs, self.outputs = [], []
 
     def add_input(self, item: Item, quantity: int):
-        self.inputs.append(RecipeItem(item=item, quantity=quantity, crafting_time=self.crafting_time))
+        self.inputs.append(RecipeComponent(item=item, quantity=quantity))
 
     def add_output(self, item: Item, quantity: int):
-        self.outputs.append(RecipeItem(item=item, quantity=quantity, crafting_time=self.crafting_time))
+        self.outputs.append(RecipeComponent(item=item, quantity=quantity))
 
     def __repr__(self):
         return f"Recipe : {self.name}"
--- tests/satisfactoryrecipe/test_recipe.py
+++ tests/satisfactoryrecipe/test_recipe.py
@@ -3,19 +3,18 @@ from src.satisfactoryrecipe.item import Item
 
 
 def test_create_recipe():
-    recipe = Recipe("recipe",None,5)
+    recipe = Recipe("recipe", None, 5)
     assert recipe is not None
 
 
 def test_add_input():
     recipe = Recipe("recipe", None, 6)
     some_item = Item("some  item", "")
-    recipe.add_input(some_item,1)
+    recipe.add_input(some_item, 1)
     assert len(recipe.inputs) == 1
     assert len(recipe.outputs) == 0
     assert recipe.inputs[0].item == some_item
     assert recipe.inputs[0].quantity == 1
-    assert recipe.inputs[0].quantity_per_minute == 10
 
 
 def test_add_output():
@@ -26,4 +25,3 @@ def test_add_output():
     assert len(recipe.outputs) == 1
     assert recipe.outputs[0].item == some_item
     assert recipe.outputs[0].quantity == 1
-    assert recipe.outputs[0].quantity_per_minute == 10
